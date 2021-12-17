67c668dfc4adbf32f7e86f576b76ee9cb929df90
rktlskan021 <rktlskan021@gmail.com>
2021-12-18 05:26:17 +0900

    백준 알고리즘 풀이 정리

--- 2941_.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    a = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-    alpha = input()
-    for t in a:
-        alpha = alpha.replace(t, '*')
-    print(len(alpha))
